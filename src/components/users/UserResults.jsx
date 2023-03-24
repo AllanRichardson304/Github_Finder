6be8658d663e43eebb22c4f5e6b7fb9b4ada42c2
allanrichardson304 <allan@nextazy.com>
2023-03-24 12:21:50 +0530

    Direct API Url Added

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,43 +1,34 @@
-import { useEffect, useState } from "react" 
+import { useEffect, useState } from "react";
 export default function UserResults() {
-
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)
-
-
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    fetchUsers()
-  }, [])
-
+    fetchUsers();
+  }, []);
   const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authoirzation: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    })
-    
-    const data = await response.json()
-    setUsers(data)  
-    setLoading(false)
-
-  }
-
+    const response = await fetch(`https://api.github.com/users`, {
+      // headers: {
+      //   Authorization: `token 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`,
+      // },
+    });
+    const data = await response.json();
+    setUsers(data);
+    setLoading(false);
+  };
   if (!loading) {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-col-4 lg:grid-col-3 md:grid-col-2 sm:grid-col-1">
-        {users.map((user) => (
-          <h3>{user.login}</h3>
+        {users.map((user, i) => (
+          <h3 key={i}>{user.login}</h3>
         ))}
       </div>
-    )
-  }
-  else {
+    );
+  } else {
     return (
       <>
         <h3>Loading....</h3>
         {/* {process.env.REACT_APP_GITHUB_TOKEN} */}
       </>
-    )
+    );
   }
-
 }
