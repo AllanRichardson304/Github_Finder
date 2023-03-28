a4110f3bc6ff921235d9c2b293fc4141f15ea368
AllanRichardson304 <allanrichardson304@gmail.com>
2023-03-28 22:34:17 +0530

    Reducer Added

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,17 +1,14 @@
-import { useEffect, useContext } from "react";
+import {useContext } from "react";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
 import GithubContext from "../../context/github/githubContext";
 export default function UserResults() {
-  const {users,loading,fetchUsers,} =useContext(GithubContext)
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+  const {users,loading,} =useContext(GithubContext)
   if (!loading) {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
-        {users.map((user, i) => (
-         <UserItem key={user.id} user={user}/> 
+        {users.map((user, id) => (
+         <UserItem key={id} user={user}/> 
         ))}
       </div>
     );
